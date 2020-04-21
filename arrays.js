7488f377b9e5abe432b3e0b649ae07d5b9867d24
Geno McNew <genemcnew3@gmail.com>
2020-04-20 19:48:33 -0500

    added arrays

---
+++ arrays.js
@@ -0,0 +1,8 @@
+const chal1 = (arr,index) => arr[index];
+
+console.log(chal1([1,2,3,4,5],3))
+
+const chal2 = (arr) => arr.includes("luna");
+
+console.log(chal2(["bob", "luna", "tim"]))
+console.log(chal2(["bob","tim"]))
